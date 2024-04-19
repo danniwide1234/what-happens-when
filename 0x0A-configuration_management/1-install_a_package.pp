0b7bd75ad57977a460679c59052e7baea5d04468
danniwide1234 <danniwide.1981@gmail.com>
2024-04-19 09:14:00 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
+  require => Package['python3-pip'],
+}
+
+file { '/usr/bin/flask':
+  ensure => link,
+  target => '/usr/local/bin/flask',
+  require => Package['flask'],
+}
+
